0a001f091caf57c20005b2c4691e49c43f3e2f59
khabyysf-dev <khabyysf@gmail.com>
2026-01-07 20:14:23 +0100

    Update google_setup.php

--- includes/google_setup.php
+++ includes/google_setup.php
@@ -5,11 +5,10 @@ require_once 'vendor/autoload.php';
 $client_id = "YOUR_CLIENT_ID";
 $client_secret = "YOUR_CLIENT_SECRET";
 $redirectUri = 'http://localhost/login/google-callback.php';
-
 $client = new Google_Client();
 $client->setClientId($clientID);
-$client->setClientSecret($clientSecret);
-$client->setRedirectUri($redirectUri);
+$client_id = '302882991968-4efjojbdcnigsvt1qah8jk2of2nm0en8.apps.googleusercontent.com';
+$client_secret = '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';
 $client->addScope("email");
 $client->addScope("profile");
 ?>
